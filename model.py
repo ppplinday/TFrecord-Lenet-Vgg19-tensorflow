4ab2c50dbc42db0992d516f8b073de650494b4ea
ppplinday <peilinzhou666@gmail.com>
2017-12-17 20:40:46 -0800

    version1.0

--- model.py
+++ model.py
@@ -11,38 +11,22 @@ from ops import conv2d, max_pool
 class Model(object):
 
     def __init__(self,
-                 config,
-                 debug_information=False,
                  is_train=True):
-        self.debug = debug_information
 
-        self.config = config
-        self.batch_size = self.config.batch_size
-        self.input_height = self.config.data_info[0]
-        self.input_width = self.config.data_info[1]
-        self.c_dim = self.config.data_info[2]
-        self.conv_info = self.config.conv_info
+        self.batch_size = 32
+        self.learning_rate = 1e-3
 
-        self.image = tf.placeholder(
-            name = 'image', dtype = tf.float32,
-            shape = [self.batch_size, self.input_height, self.input_width, self.c_dim],
-        )
+        self.build()
 
-        self.is_training = tf.placeholder_with_default(bool(is_train), [], name='is_training')
-
-        self.build(is_train=is_train)
-
-    def get_feed_dict(self, batch_chunk, step=None, is_training=None):
-    	fd = {
-    		self.image : batch_chunk['image'],
-    	}
-
-    	if is_training is not None:
-    		fd[self.is_training] = is_training
-
-    	return fd
+        self.sess = tf.InteractiveSession()
+        self.sess.run(tf.global_variables_initializer()) 
 
     def build(self, is_train=True):
+
+        self.input_image = tf.placeholder(tf.float32, [None, 3072])
+        self.images = tf.reshape(self.input_image, [-1, 32, 32, 1])
+        self.input_label = tf.placeholder(tf.float32, [None, 10])
+        self.labels = tf.cast(self.input_label, tf.int32)
 
     	net = conv2d(self.image, 6, name='conv1')
     	net = max_pool(net, name='max_pool1')
@@ -54,6 +38,15 @@ class Model(object):
     	net = fc(net, 10, name='fc2')
     	net = tf.nn.softmax(net)
 
+        self.prediction = tf.argmax(net, 1)
+        self.correct_prediction = tf.equal(tf.argmax(net, 1), tf.argmax(self.input_labels, 1))
+        self.train_accuracy = tf.reduce_mean(tf.cast(self.correct_prediction, "float"))
 
+        self.loss = slim.losses.softmax_cross_entropy(self.train_digits, self.input_labels)
+        self.train_op = tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss)
 
+    def run_single_step(self, images, labels):
+        _, accuracy, loss = self.sess.run(
+            [self.train_op, self.train_accuracy, self.loss], feed_dict={self.input_image : images, self.input_label : labels})
+        return loss, accuracy
 
--- trainer.py
+++ trainer.py
@@ -10,6 +10,32 @@ import os
 import time
 import tensorflow as tf
 from load_data import load_CIFAR10
+from model import Model
+
+class Trainer(object):
+
+	def init(self, network, dataset_xtrain, dataset_ytrain):
+		self.learning_rate = 1e-3
+		self.batch_size = 32
+		self.num_epoch = 50
+		self.num_sample = 50000
+		self.model = Model()
+
+		train()
+
+	def train():
+
+		for epoch in range(num_epoch):
+			for iter in range(num_sample // batch_size):
+				start = iter * 32
+				batch = dataset_xtrain[start, start + 32]
+				label = dataset_ytrain[start, start + 32]
+				loss, accurary = network.run_single_step(batch, label)
+
+			if epoch % 5 == 0:
+				print('[Epoch {}] Loss: {} Accurary: {}'.format(epoch, loss, accurary))
+
+		print('Done! End of training!')
 
 def main():
 	cifar10_dir = 'cifar-10-batches-py'
@@ -18,8 +44,19 @@ def main():
 	print(y_train.shape)
 	print(X_test.shape)
 	print(y_test.shape)
-	print(X_train[0])
-	print(y_train[0:10])
+
+	sess = tf.Session()
+	lenet = Model()
+	parameter_path = 'checkpoint/variable.ckpt'
+
+	saver = tf.tf.train.Saver()
+	if os.path.exists(parameter_path):
+		saver.restore(parameter_path)
+	else:
+		sess.run(tf.initialize_all_variables())
+
+	train = Trainer(lenet, X_train, y_train)
+
 
 if __name__ == '__main__':
 	main()
