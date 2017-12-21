ab6f85818b070f93a8089955e6fedee971f5eb12
ppplinday <peilinzhou666@gmail.com>
2017-12-20 17:29:11 -0800

    v1.4.0

--- model.py
+++ model.py
@@ -35,44 +35,14 @@ class Model:
 
     def build(self, is_train=True):
 
-        # with tf.variable_scope('net'):
-        #     print('[name {}] shape: {}'.format('init', self.images.shape))
-        #     net = conv2d(self.images, 6, name='conv1')
-        #     print('[name {}] shape: {}'.format('conv1', net.shape))
-        #     net = max_pool(net, name='max_pool1')
-        #     print('[name {}] shape: {}'.format('p1', net.shape))
-        #     net = conv2d(net, 16, name='conv2')
-        #     print('[name {}] shape: {}'.format('conv2', net.shape))
-        #     net = max_pool(net, name='max_pool2')
-        #     print('[name {}] shape: {}'.format('p2', net.shape))
-        #     net = conv2d(net, 120, name = 'conv3')
-        #     print('[name {}] shape: {}'.format('conv3', net.shape))
-        #     net = tf.layers.flatten(net)
-        #     print('[name {}] shape: {}'.format('fff', net.shape))
-        #     net = fc(net, 84, name='fc1')
-        #     print('[name {}] shape: {}'.format('fc1', net.shape))
-        #     net = fc(net, 10, name='fc2')
-        #     print('[name {}] shape: {}'.format('fc2', net.shape))
-        #     net = tf.nn.softmax(net)
-        #     print('[name {}] shape: {}'.format('softmax', net.shape))
-        
         with slim.arg_scope([slim.conv2d], padding='VALID', weights_initializer=tf.truncated_normal_initializer(stddev=0.02)):
-            print('[name {}] shape: {}'.format('init', self.images.shape))
             net = slim.conv2d(self.images,6,[5,5],1,scope='conv1')
-            print('[name {}] shape: {}'.format('conv1', net.shape))
             net = slim.max_pool2d(net, [2, 2], scope='pool2')
-            print('[name {}] shape: {}'.format('p2', net.shape))
             net = slim.conv2d(net,16,[5,5],1,scope='conv3')
-            print('[name {}] shape: {}'.format('conv3', net.shape))
             net = slim.max_pool2d(net, [2, 2], scope='pool4')
-            print('[name {}] shape: {}'.format('p4', net.shape))
             net = slim.conv2d(net,120,[5,5],1,scope='conv5')
-            print('[name {}] shape: {}'.format('conv5', net.shape))
             net = slim.flatten(net, scope='flat6')
-            print('[name {}] shape: {}'.format('flat6', net.shape))
             net = slim.fully_connected(net, 84, scope='fc7')
-            print('[name {}] shape: {}'.format('fc7', net.shape))
             digits = slim.fully_connected(net, 10, scope='fc8')
-            print('[name {}] shape: {}'.format('fc8', digits.shape))
         return digits
 
--- trainer.py
+++ trainer.py
@@ -39,10 +39,11 @@ class Trainer:
 					label[i][temp[i]] = 1
 				self.sess.run(self.model.train_op, feed_dict={self.model.input_image: batch, self.model.input_label: label})
 
-			if epoch % 5 == 0:
+			if epoch % 2 == 0:
 				loss, accurary = self.sess.run([self.model.loss, self.model.train_accuracy],
 					feed_dict={self.model.input_image: batch, self.model.input_label: label})
 				print('[Epoch {}] Loss: {} Accurary: {}'.format(epoch, loss, accurary))
+		save_path = saver.save(sess, parameter_path)
 
 		print('Done! End of training!')
 
