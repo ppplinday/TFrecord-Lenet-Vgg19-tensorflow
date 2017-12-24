a4dd0209a83c19fd873e2177368fcae3e28bc7c1
ppplinday <peilinzhou666@gmail.com>
2017-12-23 21:50:49 -0800

    v1.5.3

--- eval.py
+++ eval.py
@@ -9,6 +9,9 @@ from model import Model
 def main():
 	cifar10_dir = 'cifar-10-batches-py'
 	X_train, y_train, X_test, y_test = load_CIFAR10(cifar10_dir)
+	label = np.zeros((10000, 10))
+	for i in range(10000):
+		label[i][y_test[i]] = 1
 
 	sess = tf.Session()
 	model = Model()
@@ -19,7 +22,7 @@ def main():
 	print('loaded the weight')
 
 	accurary = sess.run([model.train_accuracy], 
-		feed_dict={model.input_image: X_test, model.input_label: y_test})
+		feed_dict={model.input_image: X_test, model.input_label: label)
 	print('Accurary: {}'.format(accurary))
 
 if __name__ == "__main__":
