d672ad8ec4c5aecdbee9ab2886989e45e6cc8f14
ppplinday <peilinzhou666@gmail.com>
2018-01-09 14:06:55 -0800

    v1.11.17

--- data_preprocess.py
+++ data_preprocess.py
@@ -180,7 +180,7 @@ def transform_test(inputs, mean, std, random_angle=15., pca_sigma=255., expand_r
 
 def data_preprocess(X_train, train=True, model='lenet'):
 	with tf.Session():
-		X_train = type(X_train.eval())
+		X_train = X_train.eval()
 	x_mean = np.mean([x for x in X_train], axis=(0,1,2))
 	x_std = np.std([x for x in X_train], axis=(0,1,2))
 	# x_mean = tf.reduce_mean(X_train, axis=(0,1,2))
