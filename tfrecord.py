2bbf2f7e5ea5dbb17e27536f5dd53af7289450e2
ppplinday <peilinzhou666@gmail.com>
2018-01-11 16:44:03 -0800

    v1.12.11

--- tfrecord.py
+++ tfrecord.py
@@ -3,62 +3,13 @@ import numpy as np
 import pickle as p
 import tensorflow as tf
 from load_data import load_CIFAR_batch, load_CIFAR10
+from data_preprocess import _preprocess, transform, transform_test, data_preprocess
 
 def _int64_feature(value):
 	return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 
 def _bytes_feature(value):
 	return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
-
-def convert(images, labels, name):
-	num = images.shape[0]
-	filename = name + '.tfrecords'
-	writer = tf.python_io.TFRecordWriter(filename)
-	for i in range(num):
-		img = images[i].tostring()
-		example = tf.train.Example(features=tf.train.Features(feature={
-			'label':_int64_feature(int(labels[i])),
-			'image':_bytes_feature(img)
-			}))
-		writer.write(example.SerializeToString())
-	writer.close()
-
-def read_and_decode(filename_queue):
-	reader = tf.TFRecordReader()
-	_, serialized_example = reader.read(filename_queue)
-	features = tf.parse_single_example(serialized_example,features={
-		'label':tf.FixedLenFeature([],tf.int64),
-		'image':tf.FixedLenFeature([],tf.string)
-		})
-
-	image = tf.decode_raw(features['image'],tf.uint8)
-	label = tf.cast(features['label'],tf.int32)
-
-	image.set_shape([32*32*3])
-	image = tf.reshape(image, [32, 32, 3])
-	image = tf.cast(image, tf.float32) * (1. / 255) - 0.5
-
-	return image, label
-
-def inputs(data_set, batch_size):
-	print('welcome to tfrecords')
-	if data_set == 'train':
-		file = 'train.tfrecords'
-	else:
-		file = 'validation.tfrecords'
-	print('tttttt')
-	with tf.name_scope('input') as scope:
-		filename_queue = tf.train.string_input_producer([file], num_epochs=None)
-	print('tttttt11111')
-	image, label = read_and_decode(filename_queue)
-	print('tttttt22222')
-	images, labels = tf.train.batch([image, label],
-		batch_size=batch_size,
-		num_threads = 1,
-		capacity = 10 * batch_size,
-		)
-
-	return images, labels
 
 def main():
 	image_placeholder = tf.placeholder(tf.uint8)
@@ -130,6 +81,7 @@ def test_tfrecords():
 		print(b.shape)
 		print(a)
 		print(b)
+		c = data_preprocess(a)
 		coord.request_stop()
 		coord.join(threads)
 
@@ -137,6 +89,16 @@ def test_tfrecords():
 	X_train, Y_train, X_test, Y_test = load_CIFAR10(cifar10_dir)
 	print(X_train[0])
 	print(Y_train[0])
+
+
+
+def input(data_set, batch_size):
+	reader = tf.TFRecordReader()
+	if data_set == 'train':
+		file = 'train.tfrecords'
+	else:
+		file = 'test.tfrecords'
+
 
 
 if __name__ == '__main__':
