0afd4d3f511ffd0fe0477da98b1fd8e459bd9236
ppplinday <peilinzhou666@gmail.com>
2018-01-11 17:29:25 -0800

    v1.12.26

--- eval.py
+++ eval.py
@@ -12,19 +12,13 @@ from data_preprocess import _preprocess, transform, transform_test, data_preproc
 import config
 
 def main(model_name):
-	cifar10_dir = 'cifar-10-batches-py'
-	X_train, Y_train, X_test, Y_test = load_CIFAR10(cifar10_dir)
-
-	#sess = tf.Session()
 	parameter_path = "checkpoint_" + model_name + "/variable.ckpt"
 	if model_name == "lenet":
 		print('loaded the lenet model')
-		X_test = data_preprocess(X_test, train=False, model=model_name)
 		model = Model_Lenet()
 	elif model_name == "vgg19":
 		print('loaded the vgg19 model')
 		model = Model_Vgg19()
-		X_test = data_preprocess(X_test, train=False, model=model_name)
 	else:
 		print('cannot find the checkpoint!')
 		return ;
@@ -50,15 +44,14 @@ def main(model_name):
 		coord = tf.train.Coordinator()
 		threads = tf.train.start_queue_runners(sess=sess, coord=coord)
 
-		for i in range(X_test.shape[0]):
-			print(i)
+		for i in range(10000):
 			X_test, Y_test = sess.run([images, labels])
 			X_test = data_preprocess(X_test, train=False, model=model_name)
 			Y_test = label_one_hot(Y_test, 10)
 			accurary = sess.run([model.train_accuracy], 
 				feed_dict={model.input_image: X_test, model.input_label: Y_test})
 			sum += accurary[0]
-		print('Accurary: {}'.format(sum / X_test.shape[0]))
+		print('Accurary: {}'.format(sum / 10000))
 
 		coord.request_stop()
 		coord.join(threads)
